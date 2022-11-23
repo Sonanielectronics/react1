eb6f7839dd579afae0ef95823e7337b367277a07
Sonanielectronics <100422141+Sonanielectronics@users.noreply.github.com>
2022-11-23 17:01:31 +0530

    Add files via upload

---
+++ my-react-project 2/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "./Heading";
+import Heading2 from "./Heading2";
+
+function App(){
+
+    return (
+        <Heading2></Heading2>
+    );
+
+}
+
+export default App;
